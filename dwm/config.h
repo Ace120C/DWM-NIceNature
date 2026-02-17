f0b24d8d6d51e0a493a457edd4624ef554e83c57
Ace120C <vegetotownley@proton.me>
2026-02-17 22:31:57 +0000

    fixed dwm and added swallow patch

--- dwm/config.h
+++ dwm/config.h
@@ -2,6 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
+static const int swallowfloating    = 0;
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -23,13 +24,11 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    /* class          instance  title  tags mask  isfloating  isterminal  noswallow  monitor */
+    { "Gimp",         NULL,     NULL,  0,         1,          0,          0,         -1 },
+    { "Firefox",      NULL,     NULL,  1 << 8,    0,          0,          1,         -1 },
+    { "st-256color",  NULL,     NULL,  0,         0,          1,          0,         -1 },
+    { NULL,	NULL, "Event Tester",  0,	  0,	      0,	  1,	     -1 },
 };
 
 /* layout(s) */
